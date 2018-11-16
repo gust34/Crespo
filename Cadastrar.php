23ec94e901e36bd92af25dc5ff3c8133ac5d4903
Caio Corrêa Chaves <caio.chaves@etec.sp.gov.br>
2018-11-15 23:38:55 -0200

    homepage parcialmente responsiva, com erro no banner superior

--- Cadastrar.php
+++ Cadastrar.php
@@ -6,32 +6,32 @@
 
 //Variaveis com as Informações do formulario
 
-  $nome = mysqli_real_escape_string($conexao,$_POST['nome']);
-  $tipo = mysqli_real_escape_string($conexao,$_POST['tipo']);
-  $categoria = mysqli_real_escape_string($conexao,$_POST['categoria']);
-  $bairro = mysqli_real_escape_string($conexao,$_POST['bairro']);
-  $qsuite = mysqli_real_escape_string($conexao,$_POST['qsuite']);
-  $qquarto = mysqli_real_escape_string($conexao,$_POST['qquarto']);
-  $areatotal = mysqli_real_escape_string($conexao,$_POST['areatotal']);
-  $areaconstruida = mysqli_real_escape_string($conexao,$_POST['areaconstruida']);
-  $qvaga = mysqli_real_escape_string($conexao,$_POST['qvaga']);
-  $qreformas = mysqli_real_escape_string($conexao,$_POST['qreformas']);
-  $qbanheiro = mysqli_real_escape_string($conexao,$_POST['qbanheiro']);
-  $crad = mysqli_real_escape_string($conexao,$_POST['crad']);
-  $cond = mysqli_real_escape_string($conexao,$_POST['cond']);
-  $cad1 = mysqli_real_escape_string($conexao,$_POST['cad1']);
-  $cad2 = mysqli_real_escape_string($conexao,$_POST['cad2']);
-  $cad3 = mysqli_real_escape_string($conexao,$_POST['cad3']);
-  $cad4 = mysqli_real_escape_string($conexao,$_POST['cad4']);
-  $cad5 = mysqli_real_escape_string($conexao,$_POST['cad5']);
-  $cad6 = mysqli_real_escape_string($conexao,$_POST['cad6']);
-  $cad7 = mysqli_real_escape_string($conexao,$_POST['cad7']);
-  $cad8 = mysqli_real_escape_string($conexao,$_POST['cad8']);
-  $cad9 = mysqli_real_escape_string($conexao,$_POST['cad9']);
-  $cad10 = mysqli_real_escape_string($conexao,$_POST['cad10']);
-  $descricao = mysqli_real_escape_string($conexao,$_POST['descricao']);
-  $foto = $_FILES['imagem']['tmp_name'];
-  $fototamanho = $_FILES['imagem']['size'];
+$nome = mysqli_real_escape_string($conexao,$_POST['nome']);
+$tipo = mysqli_real_escape_string($conexao,$_POST['tipo']);
+$categoria = mysqli_real_escape_string($conexao,$_POST['categoria']);
+$bairro = mysqli_real_escape_string($conexao,$_POST['bairro']);
+$qsuite = mysqli_real_escape_string($conexao,$_POST['qsuite']);
+$qquarto = mysqli_real_escape_string($conexao,$_POST['qquarto']);
+$areatotal = mysqli_real_escape_string($conexao,$_POST['areatotal']);
+$areaconstruida = mysqli_real_escape_string($conexao,$_POST['areaconstruida']);
+$qvaga = mysqli_real_escape_string($conexao,$_POST['qvaga']);
+$qreformas = mysqli_real_escape_string($conexao,$_POST['qreformas']);
+$qbanheiro = mysqli_real_escape_string($conexao,$_POST['qbanheiro']);
+$crad = mysqli_real_escape_string($conexao,$_POST['crad']);
+$cond = mysqli_real_escape_string($conexao,$_POST['cond']);
+$cad1 = mysqli_real_escape_string($conexao,$_POST['cad1']);
+$cad2 = mysqli_real_escape_string($conexao,$_POST['cad2']);
+$cad3 = mysqli_real_escape_string($conexao,$_POST['cad3']);
+$cad4 = mysqli_real_escape_string($conexao,$_POST['cad4']);
+$cad5 = mysqli_real_escape_string($conexao,$_POST['cad5']);
+$cad6 = mysqli_real_escape_string($conexao,$_POST['cad6']);
+$cad7 = mysqli_real_escape_string($conexao,$_POST['cad7']);
+$cad8 = mysqli_real_escape_string($conexao,$_POST['cad8']);
+$cad9 = mysqli_real_escape_string($conexao,$_POST['cad9']);
+$cad10 = mysqli_real_escape_string($conexao,$_POST['cad10']);
+$descricao = mysqli_real_escape_string($conexao,$_POST['descricao']);
+$foto = $_FILES['imagem']['tmp_name'];
+$fototamanho = $_FILES['imagem']['size'];
   
   // passar os parametros e arrumar o banco
   // comando
