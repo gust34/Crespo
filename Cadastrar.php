b89a7820954211b21dea62874d7a45d5e04ba088
Caio Corrêa Chaves <caio.chaves@etec.sp.gov.br>
2018-11-19 20:39:03 -0200

    Adicionado arquivo de autoloading e classe de conexão.

--- Cadastrar.php
+++ Cadastrar.php
@@ -20,15 +20,6 @@ $qbanheiro = mysqli_real_escape_string($conexao,$_POST['qbanheiro']);
 $crad = mysqli_real_escape_string($conexao,$_POST['crad']);
 $cond = mysqli_real_escape_string($conexao,$_POST['cond']);
 $cad1 = mysqli_real_escape_string($conexao,$_POST['cad1']);
-$cad2 = mysqli_real_escape_string($conexao,$_POST['cad2']);
-$cad3 = mysqli_real_escape_string($conexao,$_POST['cad3']);
-$cad4 = mysqli_real_escape_string($conexao,$_POST['cad4']);
-$cad5 = mysqli_real_escape_string($conexao,$_POST['cad5']);
-$cad6 = mysqli_real_escape_string($conexao,$_POST['cad6']);
-$cad7 = mysqli_real_escape_string($conexao,$_POST['cad7']);
-$cad8 = mysqli_real_escape_string($conexao,$_POST['cad8']);
-$cad9 = mysqli_real_escape_string($conexao,$_POST['cad9']);
-$cad10 = mysqli_real_escape_string($conexao,$_POST['cad10']);
 $descricao = mysqli_real_escape_string($conexao,$_POST['descricao']);
 $foto = $_FILES['imagem']['tmp_name'];
 $fototamanho = $_FILES['imagem']['size'];
@@ -102,10 +93,10 @@ if(mysqli_query($conexao,$comando)) {
     // Se inserir
     $_SESSION['Erro'] = "Sucesso ao cadastrar";
     //manda para a mesma tela ou outra se der bom
-    Header("Location: ola.php");
+    // Header("Location: ola.php");
 } else {
     // Se der ruim 
     $_SESSION['Erro'] = "Erro ao cadastrar";
-    Header("Location: area_restrita_cadastro.php");
+    // Header("Location: area_restrita_cadastro.php");
 }
 ?>
