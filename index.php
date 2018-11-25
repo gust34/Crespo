c76e1c7642afed31f0f902d0568e14d226bb7ba4
leozabotto <leozabotto02@gmail.com>
2018-11-25 21:26:59 -0200

    Adicionada section de contato.

--- index.php
+++ index.php
@@ -140,7 +140,45 @@
         </div>
     </div>
 
-    <div class="section contato">
+    <div class="section contato" id="contato" style="height:auto;">
+        <div class="parallax" style="height:auto; width:auto;">
+            <div class="row text-center">
+                <div class="col-sm-12">
+                    <iframe style="" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d466.2717282532684!2d-51.705369636270966!3d-20.784254438440612!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9490985e2c5c7ff3%3A0x42684e852be0d414!2sAv.+Ant%C3%B4nio+Trajano%2C+825+-+Centro%2C+Tr%C3%AAs+Lagoas+-+MS%2C+79640-310!5e0!3m2!1spt-BR!2sbr!4v1542982046192" width="100%" height="500px" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    
+                    <div class="row text-center mt-3">
+                        <div class="col-sm-12">   
+                            <span style="font-size:15pt;">
+                                 Av. Antônio Trajano, 825 <br>
+                                Centro - Três Lagoas
+                            </span>
+                        </div>
+                    </div>
+
+                    <div class="row text-center mt-3">
+                        <div class="col-sm-4">
+                        <span style="font-size:20pt; color: green;">
+                            <i class="fab fa-whatsapp"></i> <br> <span>                                                            
+                        <span style="font-size:15pt; color: black;"> (XX) XXXXX-XXXX </span>
+                        </div>
+
+                         <div class="col-sm-4">
+                        <span style="font-size:20pt; color: green;">
+                            <i class="fab fa-whatsapp"></i> <br> <span>                                                            
+                        <span style="font-size:15pt; color: black;"> (XX) XXXXX-XXXX </span>
+                        </div>
+
+                         <div class="col-sm-4">
+                        <span style="font-size:20pt; color: black;">
+                            <i class="far fa-envelope"></i> <Br>                                                        
+                        <span style="font-size:15pt;"> xxxxxxxx@xxxxx.com </span>
+                        </div>
+
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
     <?php include '_includes/footer.php'; ?>
