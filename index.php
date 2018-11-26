07f99f287f5498d66683cc8f6abe6069d2a1477c
Caio Corrêa Chaves <caio.chaves@etec.sp.gov.br>
2018-11-26 01:25:02 -0200

    Estilos na index arrumados(banner)

--- index.php
+++ index.php
@@ -11,40 +11,38 @@
     <link rel="Stylesheet" href="css/included_styles.css">
 </head>
 <body>
-    <div class="section">
-        <?php include '_includes/menu.php' ?>
+    <?php include '_includes/menu.php' ?>
 
-        <!--Carousel-->
-        <div class="carousel" style="margin-top: 65px;">
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img class="d-block w-100" src="img/cristo1.jpg">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="img/img1.jpeg">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="img/img2.jpeg">
-                    </div>
+    <!--Carousel-->
+    <div class="carousel" style="margin-top: 65px;">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="img/cristo1.jpg">
                 </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="img/img1.jpeg">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="img/img2.jpeg">
+                </div>
             </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
-        <!-- <img src="img/faixa.png" class="faixa"> -->
     </div>
+    <img src="img/faixa.png" class="faixa">
 
     <div class="section imoveis">
         <div class="busca">
